86bfcea76f91c3d0d9dfd7a2c6e9a907c89276a7
Raíces Ancestrales <raicesancestrales.es@gmail.com>
2025-04-18 02:13:02 +0200

    modifiaciones

--- api/reserva.js
+++ api/reserva.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
     }
 
     try {
-      const response = await fetch('https://script.google.com/macros/s/AKfycbwU6yGIc66D2-SZyf1x5nz7Jtpv9L_Nl2k_fMn-5kAyIdTVlF2UYWQMMuZQIlPOEXCV/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbxxGCu58jTD7Gg6UF6gMDNUG5MhX-JyNSTw7wyumQWKYABv6rwes6G2J2ZnMLwxvOXq/exec', {
         method: 'POST',
         body: formData,
         headers: formData.getHeaders(),
